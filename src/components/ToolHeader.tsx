ea30bd3a02d4ae31602b4835828abfe70f51f8e8
Evren <evren@akaytech.com>
2026-07-23 23:12:06 +0300

    0.7.6-8

--- src/components/ToolHeader.tsx
+++ src/components/ToolHeader.tsx
@@ -37,7 +37,7 @@ export default function ToolHeader({ title, subtitle, icon, iconColor = 'text-in
   }
 
   return (
-    <div className="flex-none pt-20 pb-6 pl-16 md:pl-16 pr-24 border-b border-slate-200 dark:border-slate-800 bg-white dark:bg-slate-900 shadow-sm z-10 flex justify-between items-center">
+    <div className="flex-none pt-20 pb-6 ps-16 pe-24 border-b border-slate-200 dark:border-slate-800 bg-white dark:bg-slate-900 shadow-sm z-10 flex justify-between items-center">
       <div>
         <h2 className="text-2xl font-black text-slate-800 dark:text-slate-100 flex items-center gap-2">
           <span className={iconColor}>
